e89659e67b36323cb51ca6a517a27e5308672dc1
Joy Chen <joychen5069@gmail.com>
2020-07-12 11:51:57 -0400

    changed src folder to appropriately reflect the information

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import ParticlesBg  from "particles-bg";
 
 class Header extends Component {
   render() {
 
     if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
       var name = this.props.data.name;
+      var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
@@ -17,8 +15,9 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
+
       <nav id="nav-wrap">
+
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
@@ -27,19 +26,19 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
+
       </nav>
 
       <div className="row banner">
-      
          <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h3>I'm a Raleigh-Durham based <span>{occupation}</span> {description}.</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               {networks}
             </ul>
          </div>
       </div>
