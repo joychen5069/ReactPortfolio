61e8a43303144fb553a7355aa3e587abed7243c4
Joy Chen <joychen5069@gmail.com>
2020-07-20 16:17:34 -0400

    nodemailer set up

--- server.js
+++ server.js
@@ -4,41 +4,29 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
-const express = require('express');
-const morgan = require('morgan');
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD
+	}
+});
 
+let mailOptions = {
+	from: 'kapu5069@gmail.com',
+	to: 'joychen5069@gmail.com',
+	subject: 'test',
+	text: 'email works'
+};
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-
-// If its production environment!
-if (process.env.NODE_ENV === 'production') {
-	const path = require('path');
-	// console.log('YOU ARE IN THE PRODUCTION ENV');
-	app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/'))
-	});
+transporter.sendMail(mailOptions, (err, data) => {
+	if (err) {
+		console.log(err, "error")
+	}
+	else {
+		console.log("email sent")
+	}
 }
-
-
-
-// Error handler
-app.use(function(err, req, res, next) {
-	console.log('====== ERROR =======');
-	console.error(err.stack);
-	res.status(500);
-});
-
-// Starting Server
-app.listen(PORT, () => {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+);
