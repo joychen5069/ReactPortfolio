0e542de63425af50befaa6986f56e601c485e0c3
Joy Chen <joychen5069@gmail.com>
2020-09-18 11:02:13 -0400

    hopefully fixed contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -37,54 +37,38 @@ class Contact extends Component {
 
     return (
       <section id="contact">
-
          <div className="row section-head">
-
             <div className="two columns header-col">
-
                <h1><span>Get In Touch.</span></h1>
-
             </div>
 
             <div className="ten columns">
-
                   <p className="lead">{message}</p>
-
             </div>
-
          </div>
 
          <div className="row">
             <div className="eight columns">
-
                <form id="contactForm" name="contactForm" method="POST" data-netlify="true">
 					<fieldset>
-
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" value={this.state.contactName} onChange={this.handleChange}/>
                   </div>
-
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" value={this.state.contactEmail} onChange={this.handleChange}/>
                   </div>
-
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
 						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" value={this.state.contactSubject} onChange={this.handleChange}/>
                   </div>
-
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.contactMessage}></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.contactMessage} onChange={this.handleChange}></textarea>
                   </div>
-
                   <div>
-                     <button className="submit" onClick={this.handleSubmit}>Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
+                     <button type="submit" className="submit" onClick={this.handleSubmit}>Submit</button>
                   </div>
 					</fieldset>
 				   </form>
