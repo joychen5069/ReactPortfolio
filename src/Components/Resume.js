bbd6ec75491eb259cc4b35a0d4f4b16029749df4
Joy Chen <joychen5069@gmail.com>
2022-01-31 09:34:51 -0500

    updated description to include current employment

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,17 +5,22 @@ class Resume extends Component {
 
     if(this.props.data){
       // var skillmessage = this.props.data.skillmessage;
+      var work = this.props.data.work.map(function(work){
+        return <div key={work.school}><h3>{work.school}</h3>
+        <p className="info">{work.degree} <span>&bull;</span><em className="date">{work.graduated}</em></p>
+        <p>{work.description}</p></div>
+      })
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-      <a className="projectLinks" href={work.github}>GitHub: {work.github}</a><br></br>
-      <a className="projectLinks" href={work.url}>Demo: {work.url}</a>
-            <p>{work.description}</p>
+      var project = this.props.data.project.map(function(project){
+        return <div key={project.company}><h3>{project.company}</h3>
+            <p className="info">{project.title}<span>&bull;</span> <em className="date">{project.years}</em></p>
+      <a className="projectLinks" href={project.github}>GitHub: {project.github}</a><br></br>
+      <a className="projectLinks" href={project.url}>Demo: {project.url}</a>
+            <p>{project.description}</p>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
@@ -27,6 +32,20 @@ class Resume extends Component {
 
     return (
       <section id="resume">
+
+<div className="row work">
+         <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {work}
+               </div>
+            </div>
+         </div>
+      </div>
 
       <div className="row education">
          <div className="three columns header-col">
@@ -43,14 +62,14 @@ class Resume extends Component {
       </div>
 
 
-      <div className="row work">
+      <div className="row project">
 
          <div className="three columns header-col">
             <h1><span>Projects</span></h1>
          </div>
 
          <div className="nine columns main-col">
-          {work}
+          {project}
         </div>
     </div>
 
